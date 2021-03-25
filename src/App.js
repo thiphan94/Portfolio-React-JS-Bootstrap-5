76bb5b78754db7148cb7915be8725b83fa774121
Nu Uyen Thi PHAN <thiphan94@gmail.com>
2021-03-25 15:47:35 +0100

    main heading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import Navbar from './components/inc/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Slider from '../inc/Slider';
+import {Link} from 'react-router-dom';
 
 function Home(){
     return (
         <div>
             <Slider />
-            <div className="container">
-                <div className="card mt-4">
-                    <div className="card-body">
-                        <h2>Home page</h2>
+            <section className="section">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 text-center">
+                            <h3 className="title">Welcome to my portfolio!</h3>
+                            <div className="underline mx-auto"></div>
+                            <p>As my personal project for learning React, I chose to create my own personal portfolio with React JS and Bootstrap 5</p>
+                            <Link to="/about" className="btn btn-dark shadow">Read more</Link>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </section>
+            
         </div>
         
     );
