9e53eddbdaef97f43a11676d79a010f8357712b5
Nu Uyen Thi PHAN <thiphan94@gmail.com>
2021-03-25 15:05:52 +0100

    carousel

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import Slider from '../inc/Slider';
 
 function Home(){
     return (
-        <div className="container">
-            <div className="card mt-4">
-                <div className="card-body">
-                    <h2>Home page</h2>
+        <div>
+            <Slider />
+            <div className="container">
+                <div className="card mt-4">
+                    <div className="card-body">
+                        <h2>Home page</h2>
+                    </div>
                 </div>
             </div>
         </div>
+        
     );
 
 }
